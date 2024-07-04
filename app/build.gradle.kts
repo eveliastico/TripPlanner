f72e0d255ffeaa9d3a8a6c6f16fe14fb19feb158
Jorge Estrada <jorgeaestrada45@gmail.com>
2024-07-04 10:33:46 -0700

    sdk 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "mx.itson.edu.tripplanner"
         minSdk = 26
-        targetSdk = 31
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
